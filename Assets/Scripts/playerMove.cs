9426576aba87d458e38427f8946b8474909d0af4
jburnett2 <jburnett@trinity.edu>
2016-09-10 11:18:20 -0500

    basic movement build

---
+++ Assets/Scripts/playerMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMove : MonoBehaviour {
+
+    public float moveSpeed = 6f;
+    private Rigidbody2D theRigidBody;
+
+    // Use this for initialization
+    void Start () {
+        theRigidBody = GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        theRigidBody.velocity = new Vector2(inputX * moveSpeed, inputY * moveSpeed);
+    }
+}
